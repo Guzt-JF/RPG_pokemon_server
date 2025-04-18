04c0a6c2e3c693d8b72269c1e16f301095cfbcba
Gustavo de Souza <204952369+Guzt-JF@users.noreply.github.com>
2025-04-18 18:14:26 -0300

    fix: idk3

--- middleware.js
+++ middleware.js
@@ -2,24 +2,23 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   console.log("Global middleware executed");
-  const URL_KEY = process.env.URL_KEY;
+  // const URL_KEY = process.env.URL_KEY;
 
   // Example: Add a custom header
-  const authHeader = req.headers.get("authorization");
+  // const authHeader = req.headers.get("authorization");
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
 
-  const token = authHeader.split(" ")[1];
-  console.log(token)
-  console.log(URL_KEY)
+  // const token = authHeader.split(" ")[1];
+  // console.log(token)
+  // console.log(URL_KEY)
   // if (token !== URL_KEY) {
   //   return NextResponse.json({ error: "Forbidden" }, { status: 403 });
   // }
 
   const response = NextResponse.next();
-  response.headers.set("X-Custom-Header", "MyGlobalMiddleware");
   return response;
 }
 
